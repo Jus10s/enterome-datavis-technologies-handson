e3f2cd909abd0ddad30ebb2a9c874f120ae750b9
Jus10s <justine.soetaert@kuleuven.be>
2023-03-21 15:25:20 +0100

    update

---
+++ src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
@@ -0,0 +1,6 @@
+<p><cite>"the best visual channels orderd by Mackinlay's ranking:"</cite></p>
+<ol>
+    <li style="font-style: oblique">position</li>
+    <li style="font-style: oblique">length</li>
+    <li style="font-style: oblique">angle</li>
+</ol>
