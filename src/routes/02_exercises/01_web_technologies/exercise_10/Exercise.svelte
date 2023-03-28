dd56816435e56586e8527f5e464650956e0e7e7f
Jus10s <justine.soetaert@kuleuven.be>
2023-03-28 14:50:24 +0200

    update

--- src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -1,2 +1,6 @@
-import Exercise_03 from 'Exercise.svelte'
-<div>Exercise_03</div>
+<script>
+import Exercise_03 from "src/routes/02_exercises/01_web_technologies/exercise_3/Exercise.svelte";
+import exercise_02 from "src/routes/02_exercises/01_web_technologies/exercise_2/Exercise.svelte";
+</script>
+
+<div><Exercise_03 /></div><div><Exercise_02 /></div>
