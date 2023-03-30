6ef492329b44173f8d1747dce780d5ef96332076
Jus10s <justine.soetaert@kuleuven.be>
2023-03-30 16:42:59 +0200

    Update

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -1,6 +1,22 @@
 <script>
-  let current = "";
+  let lastClickedButton = null;
+
+  const handleClick = (event) => {
+    if (lastClickedButton) {
+    lastClickedButton.classList.remove('clicked');
+  }
+
+  event.target.classList.add('clicked');
+    lastClickedButton = event.target;
+    current = event.target.dataset.player;
+  };
 </script>
+
+<style>
+  .clicked {
+    background-color: green;
+  }
+</style>
 
 <!-- We will cover buttons later on! -->
 <button class="btn btn-secondary" on:click={() => (current = "messi")}>
